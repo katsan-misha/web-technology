7517eb608282c9231c09944843732305714609ef
Misha Katsan <katsan.misha.r@gmail.com>
2018-06-09 10:05:05 +0300

    Examen

---
+++ Examen/main.js
@@ -0,0 +1,24 @@
+for(var i = 1; i < 101; i++){
+	if(i%15 == 0)console.log("FizzBuzz");
+	else if(i%5 == 0)console.log("Buzz");
+	else if(i%3 == 0)console.log("Fizz");
+	else console.log(i);
+}
+
+var i = 1;
+while(i < 101){
+	if(i%15 == 0)console.log("FizzBuzz");
+	else if(i%5 == 0)console.log("Buzz");
+	else if(i%3 == 0)console.log("Fizz");
+	else console.log(i);
+	i++;
+}
+
+var i = 1;
+do{
+	if(i%15 == 0)console.log("FizzBuzz");
+	else if(i%5 == 0)console.log("Buzz");
+	else if(i%3 == 0)console.log("Fizz");
+	else console.log(i);
+	i++;
+}while(i < 100);
